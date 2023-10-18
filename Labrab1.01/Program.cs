b79bb4f114a8d4e6e7485c10bd6c86e1a3da0df8
Anastasia <arielgerman273@gmail.com>
2023-10-18 23:11:47 +0500

    Add labrab1

---
+++ Labrab1.01/Program.cs
@@ -0,0 +1,69 @@
+﻿
+float[] array = new float[10];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+
+    array[i] = rand.NextSingle() * 20 * 1 - 10;
+    Console.WriteLine(array[i]);
+}
+
+int a = 0;
+int min_index = 0;
+while (a < array.Length)
+{
+    if (array[a] < array[min_index])
+    {
+        min_index = a;
+    }
+    a++;
+}
+Console.WriteLine("Индекс минимального элемента = " + min_index);
+
+int b = 0;
+while (b < array.Length)
+{
+    if (array[b] < 0)
+    {
+        break;
+    }
+    b++;
+}
+int c = b + 1;
+while (c < array.Length)
+{
+    if (array[c] < 0)
+    {
+        break;
+    }
+    c++;
+}
+float d = 0;
+for (int i = b + 1; i < c; i++)
+{
+    d = d + array[i];
+}
+Console.WriteLine("Сумма от первого отрицательного до второго отрицательного (не включительно) = " + d);
+
+
+int less_than_one = 0;
+int more_than_one = array.Length - 1;
+while (less_than_one < more_than_one)
+{
+    if (Math.Abs(array[less_than_one]) > 1)
+    {
+        float t = array[less_than_one];
+        array[less_than_one] = array[more_than_one];
+        array[more_than_one] = t;
+        more_than_one--;
+    }
+    else
+    {
+        less_than_one++;
+    }
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
---
+++ Labrat1.02/Program.cs
@@ -0,0 +1,81 @@
+﻿int n = 5;
+int m = 7;
+int[,] matrix = new int[n, m];
+Random rand = new Random();
+for (int i = 0; i < n; i++)
+{
+    for(int j = 0; j < m; j++)
+    { 
+        matrix[i, j] = rand.Next(-5, 5);
+        Console.Write($"{matrix[i, j],2} ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("======");
+
+
+int getFeature(int[,] matrix, int column)
+{
+    int sum = 0;
+    for(int i = 0; i < n; i+=2) 
+    {
+        sum += matrix[i, column];
+    }
+    return sum;
+}
+
+for(int i = 0; i < m; i++)
+{
+    for(int j = 0; j < m - 1; j++)
+    {
+        int f1 = getFeature(matrix, j);
+        int f2 = getFeature(matrix, j + 1);
+        if(f1 > f2)
+        {
+            for(int k = 0; k < n; k++)
+            {
+                int c = matrix[k, j];
+                matrix[k, j] = matrix[k, j + 1];
+                matrix[k, j + 1] = c;
+            }
+        }
+    }
+}
+
+
+for (int i = 0; i < n; ++i)
+{
+    for (int j = 0; j < m; ++j)
+    {
+        Console.Write($"{matrix[i, j],2} ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Характеристики:");
+for (int i = 0; i < m; i++)
+{
+    Console.Write($"{getFeature(matrix, i), 2} ");
+}
+Console.WriteLine();
+int sum_neg_cols = 0;
+for (int j = 0; j < m; j++)
+{
+    int col_sum = 0;
+    bool has_negative = false;
+    for (int i = 0; i < n; i++)
+    {
+        if (matrix[i, j] < 0)
+        {
+            has_negative = true;
+        }
+        col_sum += matrix[i, j];
+    }
+    if(has_negative)
+    {
+        sum_neg_cols += col_sum;
+    }
+}
+
+Console.WriteLine("Сумма столбцов с отрицательными элементами: " + sum_neg_cols);
