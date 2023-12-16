a83420584a5e33c5342252be670c8b902a947d23
Anastasia <arielgerman273@gmail.com>
2023-12-17 02:50:17 +0500

    Add labrab4

---
+++ labrab4/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labrab4
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Models.Composition> Compositions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite($"Data Source=db.sqlite");
+        }
+    }
+}
---
+++ labrab4/Controllers/CompositionsController.cs
@@ -0,0 +1,57 @@
+﻿using labrab4.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Xml.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace labrab4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompositionsController : ControllerBase
+    {
+        private readonly AppDbContext context;
+        public CompositionsController(AppDbContext _context)
+        {
+            this.context = _context;
+        }
+        //list - display all items in the catalog
+        //search - find items by query
+        //add - add new item
+        //del - delete item by full name
+        //save - save items to disk
+        //load - load items from disk
+        //quit - exit
+
+        // GET: api/<CompositionsController>/List
+        [HttpGet("List")]
+        public IEnumerable<Composition> Get()
+        {
+            return context.Compositions;
+        }
+
+        // GET api/<CompositionsController>/Search
+        [HttpGet("Search")]
+        public IEnumerable<Composition> Search(string query)
+        {
+            return context.Compositions.AsEnumerable().Where(x => x.SearchMatches(query));
+        }
+
+        // POST api/<CompositionsController>/Add
+        [HttpPost("Add")]
+        public void Add([FromBody] Composition value)
+        {
+            context.Compositions.Add(value);
+            context.SaveChanges();
+        }
+
+        // PUT api/<CompositionsController>/Del
+        [HttpDelete("Del")]
+        public void Delete([FromBody] Composition value)
+        {
+            context.Compositions.Remove(value);
+            context.SaveChanges();
+        }
+    }
+}
